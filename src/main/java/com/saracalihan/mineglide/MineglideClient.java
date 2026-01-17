dd61c9401d07fa6dd44dfac150f7026020a0eefb
Alihan SARAÇ(Brick) <saracalihan@gmail.com>
2026-01-17 21:45:57 +0300

    feat: init

---
+++ src/main/java/com/saracalihan/mineglide/MineglideClient.java
@@ -0,0 +1,11 @@
+package com.saracalihan.mineglide;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MineglideClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
